b2f457a2c8522197d44cb3e7457a81e1e2828eed
Cjustina <c.justina@gmail.com>
2024-01-12 12:42:25 +0200

    updated

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -5,13 +5,13 @@ from django.contrib.auth.forms import User
 class Post(models.Model):
     title = models.CharField(verbose_name="Pavadinimas", max_length=50)
     author = models.ForeignKey(to=User, verbose_name="Autorius", on_delete=models.SET_NULL, null=True)
-    data = models.DateTimeField(verbose_name="Data", auto_now_add=True)
+    date = models.DateTimeField(verbose_name="Data", auto_now_add=True)
     content = models.TextField(verbose_name="Tekstas", max_length=5000)
 
 class Comment(models.Model):
     post = models.ForeignKey(to="Post", verbose_name="Įrašas", on_delete=models.CASCADE)
     author = models.ForeignKey(to=User, verbose_name="Autorius", on_delete=models.SET_NULL, null=True)
-    data = models.DateTimeField(verbose_name="Data", auto_now_add=True)
+    date = models.DateTimeField(verbose_name="Data", auto_now_add=True)
     content = models.TextField(verbose_name="Tekstas", max_length=1000)
 
 
